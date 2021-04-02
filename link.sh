99662aa544891aed1e5e8c92853df405f7c92057
Patrick Varin <pvarin@bostondynamics.com>
2021-04-02 13:18:47 -0400

    make vimux a bash command to open vim in tmux

--- link.sh
+++ link.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Create symlinks for all of the files named *.symlink
+# Get the script directory
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	SCRIPT_DIR=$(dirname "$(greadlink -f "$0")")
+    SCRIPT_DIR=$(dirname "$(greadlink -f "$0")")
 else
-	SCRIPT_DIR=$(dirname "$(readlink "$0")")
+    SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 fi
+
+# Create symlinks for all of the files named *.symlink
 for f in $(find $SCRIPT_DIR -name '*.symlink'); do
     LINK_NAME=$HOME/.$(basename ${f%.symlink})
     ln -sf $f $LINK_NAME
 done;
 
+# Append the bashrc.append file to bashrc
+if [ -f $SCRIPT_DIR/bashrc.append && $BASHRC_APPEND != yes]; then
+    echo "[[ \$BASHRC_APPEND != yes ]] && export BASHRC_APPEND=yes && source $SCRIPT_DIR/bashrc.append" >> ~/.bashrc
+fi
